b5af4313f537a26f6a2f4b73128c4b9050d343f5
faurori <faurori@gmail.com>
2021-06-21 14:38:45 -0400

    started insert

--- GoldbrickORM/src/main/java/com/revature/repos/InsertDB.java
+++ GoldbrickORM/src/main/java/com/revature/repos/InsertDB.java
@@ -1,5 +1,6 @@
 package com.revature.repos;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,62 +21,136 @@ import com.revature.util.ConnectionUtil;
 import com.revature.util.ForeignKeyField;
 import com.revature.util.Metamodel;
 
-public class InsertDB {
+public class InsertDB<T> {
+	
+	private Class<T> clazz;
+	
+	
 	
 	private Configuration config;
 	
+	public InsertDB(Class<T> clazz) {
+		this.clazz = clazz;
+		
+	}
 	public InsertDB(Configuration cfg) { 
 		this.config = cfg;
 	}
 	
-		
 	
-	public boolean getInsertString(Connection conn) {
+	
+	public String getInsert(Connection conn) throws IllegalArgumentException, IllegalAccessException {
 		
-		for(Metamodel<Class<?>> mm : this.config.getMetamodels()) {
+		StringBuilder insertBuilder = new StringBuilder("INSERT INTO " + getTableName(clazz) + "(" );
+		StringBuilder valuesBuilder = new StringBuilder(" VALUES (");
+		
+		try {
+			Field field = clazz.getField("column");
+			ColumnField[] columns = (ColumnField[]) field.get(User.class);
 			
-			StringBuilder insertBuilder = new StringBuilder("INSERT INTO " + getTableName(mm.getClazz()) + "(" );
-			StringBuilder valuesBuilder = new StringBuilder(" VALUES (");
-			
-			for(String col : getAttributes(mm)) {
-				insertBuilder.append(col + ", ");
+			for (ColumnField column : columns) {
+				insertBuilder.append(column.getColumnName() + ", ");
 				valuesBuilder.append("?, ");
+				System.out.println(insertBuilder);
 			}
-			
-			
-			insertBuilder.deleteCharAt(insertBuilder.length() - 1);
-			insertBuilder.deleteCharAt(insertBuilder.length() - 1);
-			valuesBuilder.deleteCharAt(valuesBuilder.length() - 1);
-			valuesBuilder.deleteCharAt(valuesBuilder.length() - 1);
-			insertBuilder.append(")");
-			System.out.println(insertBuilder);
-			valuesBuilder.append(");");
-			System.out.println(valuesBuilder);
-			
-			StringBuilder finalString = insertBuilder.append(valuesBuilder);
-			
-			try {
-				PreparedStatement ps = conn.prepareStatement(finalString.toString());
-				
-				ps.execute();
-				
-				int count = 1;
-				
-				for(String col : getAttributes(mm)) {
-		//			ps.setObject(count, );
-					count++;
-				}
-				
-				
-				
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return false;
+		StringBuilder finalString = insertBuilder.append(valuesBuilder);
+		
+		
+		
+		return finalString.toString();
 	}
+	
+	public void saveToTable(T obj, Connection conn) throws IllegalArgumentException, IllegalAccessException {
+		
+		String sql = getInsert(conn);
+		
+		try {
+			Field field = clazz.getField("column");
+			
+			ColumnField[] attributes = (ColumnField[]) field.get(null);
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			int counter = 1;
+			
+			for (ColumnField att : attributes) {
+				String attName = att.getColumnName();
+				
+				Field newField = obj.getClass().getDeclaredField(attName);
+				
+				pstmt.setObject(counter, newField.get(obj));
+				counter++;
+			}
+			
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+//	public boolean getInsertString(Connection conn) {
+//		
+//		for(Metamodel<Class<?>> mm : this.config.getMetamodels()) {
+//			
+//			StringBuilder insertBuilder = new StringBuilder("INSERT INTO " + getTableName(mm.getClazz()) + "(" );
+//			StringBuilder valuesBuilder = new StringBuilder(" VALUES (");
+//			
+//			for(String col : getAttributes(mm)) {
+//				insertBuilder.append(col + ", ");
+//				valuesBuilder.append("?, ");
+//			}
+//			
+//			
+//			insertBuilder.deleteCharAt(insertBuilder.length() - 1);
+//			insertBuilder.deleteCharAt(insertBuilder.length() - 1);
+//			valuesBuilder.deleteCharAt(valuesBuilder.length() - 1);
+//			valuesBuilder.deleteCharAt(valuesBuilder.length() - 1);
+//			insertBuilder.append(")");
+//			System.out.println(insertBuilder);
+//			valuesBuilder.append(");");
+//			System.out.println(valuesBuilder);
+//			
+//			StringBuilder finalString = insertBuilder.append(valuesBuilder);
+//			
+//			try {
+//				PreparedStatement ps = conn.prepareStatement(finalString.toString());
+//				
+//				ps.execute();
+//				
+//				int count = 1;
+//				
+//				for(String col : getAttributes(mm)) {
+//		//			ps.setObject(count, );
+//					count++;
+//				}
+//				
+//				
+//				
+//				
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//		return false;
+//	}
 	
 	private String getTableName(Class<?> clazz) {
 		try {
@@ -116,14 +191,14 @@ public static void main(String[] args) {
 			connObj = datasource.getConnection();
 			
 			Configuration cfg = new Configuration();
-			cfg.addAnnotatedClass(User.class)
-			.addAnnotatedClass(Category.class)
-			.addAnnotatedClass(Product.class)
-			.addAnnotatedClass(Order.class);
-			
+		//	cfg.addAnnotatedClass(User.class)
+//			.addAnnotatedClass(Category.class)
+//			.addAnnotatedClass(Product.class)
+//			.addAnnotatedClass(Order.class);
+//			
 			
 			InsertDB dbb = new InsertDB(cfg);
-			dbb.getInsertString(connObj);
+			dbb.getInsert(connObj);
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
