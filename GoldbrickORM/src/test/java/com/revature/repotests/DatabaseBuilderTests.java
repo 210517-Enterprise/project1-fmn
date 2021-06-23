bedf99630e0fe8a886a4410f9294dac2c9497fe7
molliemorrow <mfmorrow97@gmail.com>
2021-06-23 09:23:40 -0700

    started working on tests

--- GoldbrickORM/src/test/java/com/revature/repotests/DatabaseBuilderTests.java
+++ GoldbrickORM/src/test/java/com/revature/repotests/DatabaseBuilderTests.java
@@ -1,5 +1,12 @@
 package com.revature.repotests;
 
+import org.junit.Test;
+
 public class DatabaseBuilderTests {
+	
+	@Test
+	public void testCreateTables() {
+		
+	}
 
 }
--- GoldbrickORM/src/test/java/com/revature/utiltests/MetamodelTests.java
+++ GoldbrickORM/src/test/java/com/revature/utiltests/MetamodelTests.java
@@ -1,5 +1,49 @@
 package com.revature.utiltests;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class MetamodelTests {
 
+	
+	@Before
+	public void setup() {
+		
+	}
+	
+	@Test
+	public void testCreateMetamodel_withAnnotatedClass() {
+		
+	}
+	
+	@Test
+	public void testCreateMetamodel_withUnannotatedClass() {
+		
+	}
+	
+	@Test
+	public void testGetTableName() {
+		
+	}
+	
+	@Test
+	public void testGetPrimaryKey() {
+		
+	}
+	
+	@Test
+	public void testGetAttributes() {
+		
+	}
+	
+	@Test
+	public void testGetForeignKeys() {
+		
+	}
+	
+	@Test
+	public void testGetGetters() {
+		
+	}
+	
 }
