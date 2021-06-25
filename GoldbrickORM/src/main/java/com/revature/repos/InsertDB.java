df60ee38cd082506a460748863a98d62f7132ff1
molliemorrow <mfmorrow97@gmail.com>
2021-06-24 19:15:37 -0700

    IsertDB work

--- GoldbrickORM/src/main/java/com/revature/repos/InsertDB.java
+++ GoldbrickORM/src/main/java/com/revature/repos/InsertDB.java
@@ -7,7 +7,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
