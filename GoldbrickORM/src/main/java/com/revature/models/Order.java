79ad5256c511e7748e1336815d3b94e47812b59b
faurori <faurori@gmail.com>
2021-06-26 14:58:37 -0400

    order comments

--- GoldbrickORM/src/main/java/com/revature/models/Order.java
+++ GoldbrickORM/src/main/java/com/revature/models/Order.java
@@ -11,7 +11,7 @@ import com.revature.annotations.JoinColumn;
 
 /**
  * This class defines a Order class can be mapped to a database table called orders.
- * @author Frank Aurori
+ * @author Frank Aurori, Mollie Morrow, Nick Gianino
  *
  */
 @Entity(tableName = "orders")
@@ -24,33 +24,38 @@ public class Order implements AnnotatedClass {
 	private int id;
 	
 	/**
-	 * 
+	 * Order has a column name user_id that stores the userID value and has a foreign key to the User entity, 
+	 * has a data type of integer
 	 */
 	@JoinColumn(columnName = "user_id", constraints = {
 			Constraint.FOREIGN_KEY }, dataType = "INT", reference = "REFERENCES users(id)")
 	private int userID;
 	
 	/**
-	 * 
+	 * Order has a column name product_id that stores the productID value and has a foreign key to the Product entity, 
+	 * has a data type of integer
 	 */
 	@JoinColumn(columnName = "product_id", constraints = {
 			Constraint.FOREIGN_KEY }, dataType = "INT", reference = "REFERENCES products(id)")
 	private int productID;
 
 	/**
-	 * 
+	 * @column indicates the database column name "order_date" in order table with not null constraints 
+	 * and datatype DATE
 	 */
 	@Column(columnName = "order_date", constraints = { Constraint.NOT_NULL }, dataType = "DATE")
 	private Date orderDate;
 
 	/**
-	 * 
+	 * @column indicates the database column name "total_price" in order table with not null constraints 
+	 * and datatype NUMBERIC
 	 */
 	@Column(columnName = "total_price", constraints = { Constraint.NOT_NULL }, dataType = "NUMERIC")
 	private double totalPrice;
 
 	/**
-	 * 
+	 * @column indicates the database column name "fulfilled" in order table with not null constraints 
+	 * and datatype Boolean
 	 */
 	@Column(columnName = "fulfilled", constraints = { Constraint.NOT_NULL }, dataType = "BOOLEAN")
 	private boolean fulfilled;
@@ -58,7 +63,15 @@ public class Order implements AnnotatedClass {
 	public Order() {
 
 	}
-
+	
+	/**
+	 * Constructor for Order Object
+	 * @param userID
+	 * @param productID
+	 * @param orderDate
+	 * @param totalPrice
+	 * @param fulfilled
+	 */
 	
 	public Order(int userID, int productID, Date orderDate, double totalPrice, boolean fulfilled) {
 		super();
@@ -68,7 +81,15 @@ public class Order implements AnnotatedClass {
 		this.totalPrice = totalPrice;
 		this.fulfilled = fulfilled;
 	}
-
+	/**
+	 * Constructor for Order Object
+	 * @param id
+	 * @param userID
+	 * @param productID
+	 * @param orderDate
+	 * @param totalPrice
+	 * @param fulfilled
+	 */
 	public Order(int id, int userID, int productID, Date orderDate, double totalPrice, boolean fulfilled) {
 		super();
 		this.id = id;
@@ -78,57 +99,101 @@ public class Order implements AnnotatedClass {
 		this.totalPrice = totalPrice;
 		this.fulfilled = fulfilled;
 	}
-
+	/**
+	 * Method that returns id (primary key)
+	 */
 	@Getter(name = "id")
 	public int getId() {
 		return id;
 	}
-
+	
+	/**
+	 * Method that sets the primary key Id
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	/**
+	 * Method that returns user_id (foreign key) 
+	 * @return
+	 */
 	@Getter(name = "user_id")
 	public int getUserID() {
 		return userID;
 	}
-
+	
+	/**
+	 * Method that sets user_id (foreign key) 
+	 * @param userID
+	 */
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
-
+	
+	/**
+	 * Method that returns product_id (foreign key) 
+	 * @return
+	 */
 	@Getter(name = "product_id")
 	public int getProductID() {
 		return productID;
 	}
-
+	
+	/**
+	 * Method that sets product_id (foreign key) 
+	 * @param productID
+	 */
 	public void setProductID(int productID) {
 		this.productID = productID;
 	}
-
+	
+	/**
+	 * method that returns order_date
+	 * @return
+	 */
 	@Getter(name = "order_date")
 	public Date getOrderDate() {
 		return orderDate;
 	}
-
+	
+	/**
+	 * method that sets the order_date
+	 * @param orderDate
+	 */
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
 
+	/**
+	 * method that returns the total price
+	 * @return
+	 */
 	@Getter(name = "total_price")
 	public double gettotalPrice() {
 		return totalPrice;
 	}
-
+	
+	/**
+	 * method that sets the total price
+	 * @param totalPrice
+	 */
 	public void settotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
 
+	/**
+	 * method that returns if a order is fulfilled 
+	 * @return
+	 */
 	@Getter(name = "fulfilled")
 	public boolean isFulfilled() {
 		return fulfilled;
 	}
-
+	/**
+	 * method that sets if a order is fulfilled
+	 * @param fulfilled
+	 */
 	public void setFulfilled(boolean fulfilled) {
 		this.fulfilled = fulfilled;
 	}
